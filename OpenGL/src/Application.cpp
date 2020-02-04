7c4b1de7689cacbf7661535a5b4f6ca49d5efa44
GrandNewStart <hbjw123@naver.com>
2020-02-04 18:08:50 +0900

    Vertex Attributes and Layouts in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -33,11 +33,12 @@ int main(void)
          0.0f,  0.5f, 
          0.5f, -0.5f 
     };
-
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
